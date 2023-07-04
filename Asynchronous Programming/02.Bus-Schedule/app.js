2fd59b8f31051f9af573bbad2e38d96511ce9878
dsimplichev <dsimplichev1948@gmail.com>
2023-07-04 23:27:31 +0100

    Async

--- Asynchronous Programming/02.Bus-Schedule/app.js
+++ Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,26 @@
 function solve() {
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const spanEl = document.querySelector('.info');
 
-    function depart() {
-        console.log('Depart TODO...');
+    let stop = {
+        next: 'depot'
+    };
+
+
+    async function depart() {
+        const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${stop.next}`);
+        const data = await response.json();
+        stop = data;
+        spanEl.textContent = `Next stop ${stop.name}`;
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        spanEl.textContent = `Arriving at ${stop.name}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
