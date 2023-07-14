7891f7cf744cf4af5bde1d5f9432288ef19d7da9
dsimplichev <dsimplichev1948@gmail.com>
2023-07-14 16:57:50 +0100

    Remote Data and Authentication

--- Remote Data and Authentication/01.Messenger/app.js
+++ Remote Data and Authentication/01.Messenger/app.js
@@ -1,5 +1,48 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+   
+    document.getElementById('submit').addEventListener('click', sendMessage);
+    document.getElementById('refresh').addEventListener('click', getMessages);
 
+    const url = 'http://localhost:3030/jsonstore/messenger'
+
+    async function sendMessage() {
+        
+        const author = document.querySelector('#controls input[name="author"]').value;
+        const content = document.querySelector('#controls input[name="content"]').value;
+         
+        if (author == '' || content == '') {
+            return alert ('All field are requared');
+        }
+        
+        const respons = await fetch (url, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                author,
+                content
+            })
+        });
+        document.querySelector('#controls input[name="author"]').value = '';
+        document.querySelector('#controls input[name="content"]').value = '';
+        const data = await respons.json();
+
+    }
+    async function getMessages() {
+        const respons = await fetch(url, {
+            method: 'get'
+        });
+
+        const data = await respons.json();
+        printData(data);
+    }
+    function printData(data) {
+        const messages = document.getElementById('messages');
+        messages.value = '';
+        Object.values(data).map(el => messages.value += `${el.author}: ${el.content}\n`);
+    }
+    }
+
+}
 attachEvents();
